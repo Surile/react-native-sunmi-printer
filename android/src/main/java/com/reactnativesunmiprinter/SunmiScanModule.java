b0e7785321578c06fc1f76774f1431b6b04ab3fd
夜雨 <mhecy98@gmail.com>
2021-12-07 18:28:02 +0800

    fix: 更新扫码bug

--- android/src/main/java/com/reactnativesunmiprinter/SunmiScanModule.java
+++ android/src/main/java/com/reactnativesunmiprinter/SunmiScanModule.java
@@ -18,58 +18,58 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class SunmiScanModule extends ReactContextBaseJavaModule {
-   private static ReactApplicationContext reactContext;
-    private static final int START_SCAN = 0x0000;
-    private static final String E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST";
-    private static final String E_FAILED_TO_SHOW_SCAN = "E_FAILED_TO_SHOW_SCAN";
-    private Promise mPickerPromise;
+  private static ReactApplicationContext reactContext;
+  private static final int START_SCAN = 0x0000;
+  private static final String E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST";
+  private static final String E_FAILED_TO_SHOW_SCAN = "E_FAILED_TO_SHOW_SCAN";
+  private Promise mPickerPromise;
 
-    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
-        @Override
-        public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
-            if (intent != null) {
-                Bundle bundle = intent.getExtras();
-                ArrayList<HashMap<String, String>> result = (ArrayList<HashMap<String, String>>) bundle.getSerializable("data");
-                Iterator<HashMap<String, String>> it = result.iterator();
-                while (it.hasNext()) {
-                    HashMap hashMap = it.next();
-                    sendEvent(hashMap.get("VALUE").toString());
-                }
-            }
-        }
-    };
-
-    public(ReactApplicationContext context) {
-     super(context);
-     reactContext = context;
-     reactContext.addActivityEventListener(mActivityEventListener);
-    }
-
+  private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
     @Override
-    public String getName() {
-        return "SunmiScanModule";
-    }
-
-    @ReactMethod
-    public void scan(final Promise promise) {
-        Activity currentActivity = getCurrentActivity();
-        if (currentActivity == null) {
-            promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
-            return;
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent intent) {
+      if (intent != null) {
+        Bundle bundle = intent.getExtras();
+        ArrayList<HashMap<String, String>> result = (ArrayList<HashMap<String, String>>) bundle.getSerializable("data");
+        Iterator<HashMap<String, String>> it = result.iterator();
+        while (it.hasNext()) {
+          HashMap hashMap = it.next();
+          sendEvent(hashMap.get("VALUE").toString());
         }
-        mPickerPromise = promise;
-        try {
-            Intent intent = new Intent("com.sunmi.scan");
-            intent.setPackage("com.sunmi.sunmiqrcodescanner");
-            intent.putExtra("PLAY_SOUND", true);
-            currentActivity.startActivityForResult(intent, START_SCAN);
-        } catch (Exception e) {
-            mPickerPromise.reject("E_FAILED_TO_SHOW_SCAN", e);
-            mPickerPromise = null;
-        }
+      }
     }
+  };
 
-    private static void sendEvent(String msg) {
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onScanSuccess", msg);
+  public SunmiScanModule(ReactApplicationContext context) {
+    super(context);
+    reactContext = context;
+    reactContext.addActivityEventListener(mActivityEventListener);
+  }
+
+  @Override
+  public String getName() {
+    return "SunmiScanModule";
+  }
+
+  @ReactMethod
+  public void scan(final Promise promise) {
+    Activity currentActivity = getCurrentActivity();
+    if (currentActivity == null) {
+      promise.reject(E_ACTIVITY_DOES_NOT_EXIST, "Activity doesn't exist");
+      return;
     }
+    mPickerPromise = promise;
+    try {
+      Intent intent = new Intent("com.sunmi.scan");
+      intent.setPackage("com.sunmi.sunmiqrcodescanner");
+      intent.putExtra("PLAY_SOUND", true);
+      currentActivity.startActivityForResult(intent, START_SCAN);
+    } catch (Exception e) {
+      mPickerPromise.reject("E_FAILED_TO_SHOW_SCAN", e);
+      mPickerPromise = null;
+    }
+  }
+
+  private static void sendEvent(String msg) {
+    reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit("onScanSuccess", msg);
+  }
 }
